24a9ff60b0e03515b9e4c0f733cac31d1e9e7c5b
zhangweiguang <zhangweiguang@kkworld.com>
2022-09-21 23:50:19 +0800

    库存接口返回值需要解密

--- src/api.ts
+++ src/api.ts
@@ -6,6 +6,7 @@ import type {
   ShopListResponse,
   RefreshTokenResponse,
 } from "./types";
+import { decrypt, dealStrSub } from "./utils";
 
 export async function getSkuStock(sku: string, shopno: string, userId: string) {
   const formData = new FormData();
@@ -17,9 +18,29 @@ export async function getSkuStock(sku: string, shopno: string, userId: string) {
     "https://e-gw.giant.com.cn/index.php/api/sku_stock",
     formData
   );
+  if (resp.data.status == 1) {
+    let dealStr = dealStrSub(resp.data.data)
+    // 展示存量
+    let result = JSON.parse(decrypt(dealStr, 'nKB6qnkQimMG5Pv1CCPfz205YgQurfcZs1kZuuDtyim8EXmR', true))
+    resp.data.data = result
+    // result.type
+    // result.ava_stock
+    // result.stock
+    // //是否限量
+    // if(_this.resultString.type == 1 ){
+    //   _this.goodsDecision.islimitbuy = 1;
+    //   _this.goodsDecision.ava_stock = _this.resultString.ava_stock;
+    // } else {
+    //   _this.goodsDecision.islimitbuy = 0;
+    //   _this.goodsDecision.ava_stock = 0;
+    // }
+  }
   return resp.data as SkuStockResponse;
 }
 
+// sku: 2251101224,2251101424,2251101324,2251101124
+// province:110000, city: 1, area：空
+// perpage=10
 export async function getShopList(page: number, options: ShopListOptions) {
   const { province, city, area, perPage = 10 } = options;
   const formData = new FormData();
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import CryptoJS from 'crypto-js'
+
 function sleep(timeout: number) {
   return new Promise((resolve) => setTimeout(resolve, timeout));
 }
@@ -49,4 +51,46 @@ export function parseList(str: string | undefined) {
     return str.split(",");
   }
   return [];
+}
+// import CryptoJS from  'crypto-js';
+// 密法：string-字段  code-约定的密钥    operation 默认false表示加，传入true表示解
+export function decrypt(string: string, code: string, operation: boolean) {
+  code = CryptoJS.MD5(code).toString()
+  var iv = CryptoJS.enc.Utf8.parse(code.substring(0, 16))
+  var key = CryptoJS.enc.Utf8.parse(code.substring(16))
+  if (operation) {
+    let decrypt = CryptoJS.AES.decrypt(string, key, {
+      iv: iv,
+      padding: CryptoJS.pad.Pkcs7
+    }).toString(CryptoJS.enc.Utf8)
+    return decrypt
+  }
+  let result = CryptoJS.AES.encrypt(string, key, {
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+  }).toString()
+  return result
+}
+// 处理返回字符串截取方法
+export function dealStrSub(val: string) {
+  let numA = Math.ceil(val.length / 2);
+  let str1 = val.substring(numA, val.length);
+  let str2 = val.substring(0, numA);
+  let str3 = str1.substring(1, 3);
+  let str3_1 = str1.substring(1, 2);
+  let str4 = str1.substring(4, 6);
+  let str4_1 = str1.substring(4, 5);
+  if (str3_1 === '0') {
+    str3 = str1.substring(2, 3)
+  }
+  if (str4_1 === '0') {
+    str4 = str1.substring(5, 6)
+  }
+  parseInt
+  let str7 = str1.substring(6, str1.length)
+  let str5 = str2 + str7;
+  let str6 = str5.substring(parseInt(str4, 10), str5.length - parseInt(str3, 10))
+  // console.log(str6)
+  return str6
 }
